6b8602b6b453bab9bb32c558c21780e42028f8bf
Max16 <max16@naver.com>
2021-07-24 11:15:18 +0900

    #15.0 Local Only Fields

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,22 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+
+export const isLoggedInVar = makeVar(false);
 
 export const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      // local state 를 cache 에 저장하는 법
+      // DB 에 접근하지 않고 browser 에서 쓸 수 있는 query 생성
+      Query: {
+        fields: {
+          isLoggedIn: {
+            read() {
+              return isLoggedInVar();
+            },
+          },
+        },
+      },
+    },
+  }),
 });
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,1 +1,12 @@
-export const LoggedInRouter = () => <span>"Logged In"</span>;
+import { isLoggedInVar } from "../apollo";
+export const LoggedInRouter = () => {
+  const onClick = () => {
+    isLoggedInVar(false);
+  };
+  return (
+    <div>
+      <h1>Logged In</h1>
+      <button onClick={onClick}>Click to Log Out</button>
+    </div>
+  );
+};
