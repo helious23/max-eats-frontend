31e7c0de4dda29a0cca46410687c86ec926d0e92
Max16 <max16@naver.com>
2021-07-24 12:20:01 +0900

    #15.1 React Hook Form

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,12 +1,50 @@
-import { isLoggedInVar } from "../apollo";
+import { useForm } from "react-hook-form";
+
 export const LoggedOutRouter = () => {
-  const onClick = () => {
-    isLoggedInVar(true);
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = () => {
+    console.log(watch("email"));
+  };
+  const onInvalid = () => {
+    console.log(errors);
+    console.log("cant log in");
   };
   return (
     <div>
       <h1>Logged Out</h1>
-      <button onClick={onClick}>Click to login</button>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            {...register("email", {
+              required: "This is required",
+              pattern: {
+                value:
+                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@gmail.com$/,
+                message: "Email is invalid",
+              },
+            })}
+            className="border border-black"
+            name="email"
+            type="email"
+            placeholder="e-mail"
+          />
+        </div>
+        <div>
+          <input
+            {...register("password", { required: "This is required" })}
+            className="border border-blue-500"
+            name="password"
+            type="password"
+            placeholder="password"
+          />
+        </div>
+        <button className="bg-yellow-300 text-white">Submit</button>
+      </form>
     </div>
   );
 };
