d053db660fc91e54b5f036c57ffb9e2998fc61be
Max16 <max16@naver.com>
2021-07-28 09:57:15 +0900

    #15.15 Using the Token

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,39 +1,58 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { LOCALSTORAGE_TOKEN } from "./constant";
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 export const isLoggedInVar = makeVar(Boolean(token));
-export const authToken = makeVar(token);
-
-console.log(isLoggedInVar());
-console.log(authToken());
+export const authTokenVar = makeVar(token);
 
 export const logUserIn = (token: string) => {
   localStorage.setItem(LOCALSTORAGE_TOKEN, token);
-  authToken(token);
+  authTokenVar(token);
   isLoggedInVar(true);
 };
 
-export const client = new ApolloClient({
+export const logUserOut = () => {
+  localStorage.removeItem(LOCALSTORAGE_TOKEN);
+  isLoggedInVar(false);
+};
+
+const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      "x-jwt": authTokenVar() || "", // 모든 request 에서 실행되므로 로그아웃 시 token 값 null 인 경우 대비
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache({
-    // typePolicies: {
-    //   // local state 를 cache 에 저장하는 법
-    //   // DB 에 접근하지 않고 browser 에서 쓸 수 있는 query 생성
-    //   Query: {
-    //     fields: {
-    //       isLoggedIn: {
-    //         read() {
-    //           return isLoggedInVar();
-    //         },
-    //       },
-    //       token: {
-    //         read() {
-    //           return authToken();
-    //         },
-    //       },
-    //     },
-    //   },
-    // },
+    typePolicies: {
+      Query: {
+        fields: {
+          isLoggedIn: {
+            read() {
+              return isLoggedInVar();
+            },
+          },
+          token: {
+            read() {
+              return authTokenVar();
+            },
+          },
+        },
+      },
+    },
   }),
 });
