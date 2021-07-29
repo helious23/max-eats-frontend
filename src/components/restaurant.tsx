c239a0bb026bea959008e516c30da0f45a78ce38
Max16 <max16@naver.com>
2021-07-29 22:15:04 +0900

    #17.3 Restaurants pagination

--- src/components/restaurant.tsx
+++ src/components/restaurant.tsx
@@ -8,7 +8,7 @@ export const Restaurant: React.FC<IRestaurantProps> = ({ restaurant }) => (
   <div className="flex flex-col">
     <div
       style={{ backgroundImage: `url(${restaurant.coverImg})` }}
-      className="bg-red-500 py-32 bg-cover bg-center mb-3"
+      className="py-32 bg-cover bg-center mb-3"
     ></div>
     <h3 className="text-xl font-medium">{restaurant.name}</h3>
     <span className="border-t mt-2 pt-2 border-gray-400 text-sm opacity-50">
--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -6,6 +6,13 @@ import {
 } from "../../__generated__/restaurantsPageQuery";
 import { Categories } from "../../components/categories";
 import { Restaurant } from "../../components/restaurant";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRight,
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const RESTAURANTS_QUERY = gql`
   query restaurantsPageQuery($input: RestaurantsInput!) {
@@ -40,37 +47,68 @@ const RESTAURANTS_QUERY = gql`
 `;
 
 export const Restaurants: React.FC = () => {
-  const { data, loading, error } = useQuery<
+  const [page, setPage] = useState(1);
+  const { data, loading } = useQuery<
     restaurantsPageQuery,
     restaurantsPageQueryVariables
   >(RESTAURANTS_QUERY, {
     variables: {
       input: {
-        page: 1,
+        page,
       },
     },
   });
+
+  const onNextPageClick = () => setPage((current) => current + 1);
+  const onPrevPageClick = () => setPage((current) => current - 1);
   return (
     <div>
       <PageTitle title={"온라인으로 음식을 주문하세요"} />
-      <form className="bg-gray-800 w-full py-40 flex items-center justify-center">
+      <form className="bg-gray-800 w-full py-32 flex items-center justify-center">
         <input
-          className="input w-4/12 rounded-md border-0"
+          className="input w-6/12 rounded-md border-0"
           type="search"
           placeholder="식당을 검색하세요..."
         />
       </form>
       {!loading && (
-        <div className="max-w-screen-2xl mx-auto mt-8">
+        <div className="max-w-screen-xl mx-auto mt-8 pb-20">
           <div className="flex justify-around max-w-xl mx-auto">
             {data?.allCategories.categories?.map((category) => (
               <Categories category={category} />
             ))}
           </div>
-          <div className="grid grid-cols-3 gap-x-5 gap-y-10 mt-10">
+          <div className="grid grid-cols-3 gap-x-5 gap-y-10 my-16">
             {data?.restaurants.results?.map((restaurant) => (
               <Restaurant restaurant={restaurant} />
             ))}
+          </div>
+          <div className="grid grid-cols-3 text-center max-w-md items-center justify-center mx-auto mt-10">
+            {page > 1 ? (
+              <div className="flex justify-center">
+                <FontAwesomeIcon
+                  icon={faChevronLeft}
+                  className="focus:outline-none text-xl cursor-pointer"
+                  onClick={onPrevPageClick}
+                />
+              </div>
+            ) : (
+              <div></div>
+            )}
+            <span className="flex justify-center">
+              Page {page} of {data?.restaurants.totalPages}
+            </span>
+            {page !== data?.restaurants.totalPages ? (
+              <div className="flex justify-center">
+                <FontAwesomeIcon
+                  icon={faChevronRight}
+                  className="focus:outline-none text-xl cursor-pointer"
+                  onClick={onNextPageClick}
+                />
+              </div>
+            ) : (
+              <div></div>
+            )}
           </div>
         </div>
       )}
