6446f67b7af5d24522f4047c0e84edba66cca69e
Max16 <max16@naver.com>
2021-07-31 09:03:33 +0900

    #18.1 App Tests

--- src/components/__tests__/app.spec.tsx
+++ src/components/__tests__/app.spec.tsx
@@ -2,6 +2,9 @@ import { render, waitFor } from "@testing-library/react";
 import { App } from "../app";
 import { isLoggedInVar } from "../../apollo";
 
+// app.tsx 에서는 LoggedOutRouter 와 LoggedInRouter 가 존재하는지만 확인하면 됨
+// 작동 여부는 추후 login testing 에서 확인
+
 jest.mock("../../routers/logged-out-router.tsx", () => {
   return {
     LoggedOutRouter: () => <span>logged-out</span>,
@@ -16,13 +19,14 @@ jest.mock("../../routers/logged-in-router.tsx", () => {
 
 describe("<App />", () => {
   it("renders LoggedOutRouter", () => {
-    const { getByText } = render(<App />);
-    getByText("logged-out");
+    const { getByText } = render(<App />); // render 함수로 App component 를 넘겨줌
+    getByText("logged-out"); // text 가 있는지 확인
   });
   it("renders LoggedInRouter", async () => {
     const { getByText } = render(<App />);
     await waitFor(() => {
-      isLoggedInVar(true);
+      // state 가 바뀌고 refresh 할 때 까지 기다려줌
+      isLoggedInVar(true); // 실제 로그인 하지 않고 react variable 만 변경
     });
     getByText("logged-in");
   });
