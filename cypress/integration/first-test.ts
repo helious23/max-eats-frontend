25de5f013ffc669c4aa4d8ec80b169d6d911217e
Max16 <max16@naver.com>
2021-08-02 15:45:35 +0900

    #19.1 Our First Cypress Test

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -4,12 +4,15 @@ describe("Log In", () => {
     cy.visit(routes.home).title().should("eq", "로그인 | Max Eats");
   });
   it("can fill out the form", () => {
-    cy.visit(routes.home)
-      .get('[name="email"]')
-      .type("jenny@gmail.com")
-      .get('[name="password"]')
-      .type("12345")
-      .get(".grid > .text-white")
-      .should("not.have.class", "pointer-events-none");
+    cy.visit(routes.home);
+    cy.findByPlaceholderText("이메일").type("jenny@gmail.com");
+    cy.findByPlaceholderText("패스워드").type("12345");
+    cy.findByRole("button").should("not.have.class", "pointer-events-none");
+    // to do (can log in)
+  });
+  it("can see email / password validation error", () => {
+    cy.visit(routes.home);
+    cy.findByPlaceholderText("이메일").type("bad@email");
+    cy.findByRole("alert").should("have.text", "유효한 이메일 주소가 아닙니다");
   });
 });
